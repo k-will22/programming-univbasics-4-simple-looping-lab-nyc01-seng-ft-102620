7221c9199a899b9a2000a86792f8bcaec0d52ba1
Keith Williams <thestreetkw@hotmail.com>
2020-10-11 06:08:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,8 +26,9 @@ end
 
 def return_string_array(array)
   counter = 0 
+  new_array = []
   while counter < array.length do 
-    p array[counter].to_s 
+    new_array.push(array[counter].to_s)
     counter += 1 
   end 
 end 
